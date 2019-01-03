ce741f2207e84fd76d3031a7e0d9fb354b2dd63d
Ronaldo <kikogamer@gmail.com>
2019-01-03 21:51:14 -0200

    calculating the total

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -4,11 +4,16 @@ class Sell < ApplicationRecord
 
   validates :client, presence: true
 
+  belongs_to :discount
+  belongs_to :client
+
   has_many :sell_products
   has_many :products, through: :sell_products
 
   has_many :sell_services
   has_many :services, through: :sell_services
+
+  before_save :set_total
   
   def fae_display_field
     id
@@ -18,6 +23,18 @@ class Sell < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
+  private
+
+  def set_total
+    total = 0
+    self.products.each {|p| total += p.price }
+    self.services.each {|s| total += s.price }
+
+    if self.discount.present?
+      total = total - self.discount.value
+    end
+
+    total = (total >= 0)? total : 0
+    self.total = total
+  end
 end
